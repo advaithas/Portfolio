28f648c2134ac52aab6f2a002aaa6f42b034e280
Advaitha <92393310+advaithas@users.noreply.github.com>
2023-08-15 17:18:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -49,26 +49,3 @@ router.post("/contact", (req, res) => {
   });
 });
 
-router.get("/contact", (req, res) => {
-  const name = req.body.firstName + req.body.lastName;
-  const email = req.body.email;
-  const message = req.body.message;
-  const phone = req.body.phone;
-  const mail = {
-    from: name,
-    to: "advaitha200324@gmail.com",
-    subject: "Contact Form Submission - Portfolio",
-    html: `<p>Name: ${name}</p>
-           <p>Email: ${email}</p>
-           <p>Phone: ${phone}</p>
-           <p>Message: ${message}</p>`,
-  };
-  contactEmail.sendMail(mail, (error) => {
-    if (error) {
-      res.json(error);
-    } else {
-      res.json({ code: 200, status: "Message Sent" });
-    }
-  });
-});
-
